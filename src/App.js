eebfd518b0bd37024997d7cb9f927b79d6f9e465
omarmendozaaa <mendozavallejosomar@gmail.com>
2021-06-18 23:44:19 -0500

    completando el state de la predicción

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import FormPrediction from "./Components/FormPrediction";
 function App() {
+  const [predict, setPredict] = useState(-1)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Clasificación de Feminicidios utilizando K Means</h1>
+        <FormPrediction func={setPredict}/>
+        <p>Cluster al que pertenece: {predict + 1}</p>
       </header>
     </div>
   );
