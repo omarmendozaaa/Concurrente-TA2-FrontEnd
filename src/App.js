4d50e34452af082a8646daf0ce77ca227921657c
omarmendozaaa <mendozavallejosomar@gmail.com>
2021-06-19 02:32:35 -0500

    terminando frontend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import FormPrediction from "./Components/FormPrediction";
+import Charts from "./Components/Charts";
 function App() {
   const [predict, setPredict] = useState(-1)
   return (
@@ -9,6 +10,7 @@ function App() {
         <h1>Clasificación de Feminicidios utilizando K Means</h1>
         <FormPrediction func={setPredict}/>
         <p>Cluster al que pertenece: {predict + 1}</p>
+        <Charts className="Charts"/>
       </header>
     </div>
   );
